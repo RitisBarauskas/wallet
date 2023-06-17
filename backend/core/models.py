611114dc7abf81d58cb2ed89d872aee867de64b9
Ritis Barauskas <perm300ru@yandex.ru>
2023-06-17 23:27:20 +0300

    upd soft deleted model

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,6 +1,7 @@
 import uuid
 
 from django.db import models
+from django.utils import timezone
 
 
 class UUIDMixin(models.Model):
@@ -41,6 +42,15 @@ class SoftDeleteMixin(models.Model):
     is_deleted = models.BooleanField(
         default=False,
     )
+    deleted_at = models.DateTimeField(
+        null=True,
+        blank=True,
+    )
 
     class Meta:
         abstract = True
+
+    def delete(self, *args, **kwargs):
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save()
